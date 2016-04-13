a7d8b7ddd110ff91650562f88733ce6a9f649997
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-13 10:57:13 +0200

    Minor cleanup

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,9 +1,12 @@
+// ES6 imports
 import IndexComponent from './components/pages/index.es6.js';
 import IndexSecondComponent from './components/pages/indexSecond.es6.js';
 import ShowComponent from './components/pages/show.es6.js';
 
+// Setup a global app scope
 var app = window.app = global.app = {};
 
+// Expose components to global scope
 app.IndexComponent = IndexComponent;
 app.IndexSecondComponent = IndexSecondComponent;
 app.ShowComponent = ShowComponent;
