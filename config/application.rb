36ef1e85eb4ba589773bb1635066923d0cc65486
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-13 09:51:51 +0200

    Add babelify process to compile jsx

--- config/application.rb
+++ config/application.rb
@@ -18,8 +18,6 @@ Bundler.require(*Rails.groups)
 
 module BrowserifyOnRails
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.browserify_rails.commandline_options = "-t babelify --extension=\".jsx\""
   end
 end
