760de55627110d037ceaf2e6c22d74803c995938
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-13 09:53:13 +0200

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
+  root to: 'pages#index'
+  get 'pages/show', to: 'pages#show', as: :page_show
+  get '/ping', to: 'messages#ping', as: :ping
   # Serve websocket cable requests in-process
-  # mount ActionCable.server => '/cable'
+  mount ActionCable.server => '/cable'
 end
