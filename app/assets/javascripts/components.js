7dd725737ba839dbf3e5b5f6a8f40646a39d1bdb
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2016-04-19 18:29:27 +0300

    Add deps example to showcase it works

--- app/assets/javascripts/components.js
+++ app/assets/javascripts/components.js
@@ -1,13 +1,16 @@
 // ES6 imports
+const ReactDOMServer = window.ReactDOMServer = global.ReactDOMServer = require('react-dom/server');
+
 import IndexComponent from './components/pages/index.es6.js';
 import IndexSecondComponent from './components/pages/indexSecond.es6.js';
 import ShowComponent from './components/pages/show.es6.js';
-const ReactDOMServer = window.ReactDOMServer = global.ReactDOMServer = require('react-dom/server');
+import MapComponent from './components/pages/leaflet.es6.js';
 
 // Setup a global app scope
 const app = window.app = global.app = {};
 
 // Expose components to global scope
 app.IndexComponent = IndexComponent;
+app.MapComponent  = MapComponent;
 app.IndexSecondComponent = IndexSecondComponent;
 app.ShowComponent = ShowComponent;
---
+++ app/assets/javascripts/components/pages/leaflet.es6.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
+const position = [51.505, -0.09];
+
+class MapComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return(
+      <Map center={position} zoom={13}>
+        <TileLayer
+          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        />
+        <Marker position={position}>
+          <Popup>
+            <span>A pretty CSS3 popup.<br/>Easily customizable.</span>
+          </Popup>
+        </Marker>
+      </Map>
+    );
+  }
+}
+
+module.exports = MapComponent;
